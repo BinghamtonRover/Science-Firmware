db4d98ed55a8a1bb2a9da08f51f38ba76983b236
Levi Lesches <levilesches@gmail.com>
2022-03-10 02:28:05 -0500

    Enabled pumps and disabed auger in Science.ino

--- Science/Science.ino
+++ Science/Science.ino
@@ -2,6 +2,8 @@
 // Also includes <science_motors.h> 
 #include <science_pinouts.h>
 
+/* This script controls everything except for the Auger. */
+
 // #define AUGER_FAST_SPEED 50
 // #define AUGER_DROP_SPEED -25
 // #define AUGER_DIG_DELAY 1000 // ms
@@ -15,8 +17,8 @@
 
 #define SENSORS_READ_DELAY 1000  // ms
 
-#define PUMP_SPEED 0
-#define PUMP_DELAY 0  // ms
+#define PUMP_SPEED 100
+#define PUMP_DELAY 10000  // 2000  // ms
 
 void block() {
 	while (!Serial.available());
@@ -57,22 +59,22 @@ void calibrate() {
 	dirtCarousel.calibrate();
 	dirtLinear.calibrate();
 	scienceLinear.calibrate();
-	augerLinear.calibrate();  // WARNING: will go through the table!
+	// augerLinear.calibrate();  // WARNING: will go through the table!
 }
 
 void dig() {  // all motor movements are blocking
 	// Lower and dig
-	dirtLinear.setPosition(0);
-	augerLinear.setPosition(0);
+	// dirtLinear.setPosition(0);
+	// augerLinear.setPosition(0);
 
 	// auger.setSpeed(AUGER_FAST_SPEED); 
 	// delay(AUGER_DIG_DELAY); 
 	// auger.softBrake();
-	Serial.println("Press Enter when the digging is finished.");
-	block();
+	// Serial.println("Press Enter when the digging is finished.");
+	// block();
 
 	// Lift and dump
-	augerLinear.setPosition(AUGER_LINEAR_MAX_HEIGHT);
+	// augerLinear.setPosition(AUGER_LINEAR_MAX_HEIGHT);
 	dirtLinear.setPosition(DIRT_LINEAR_OUTER_RADIUS);
 
 	// Drop in each tube.
@@ -80,23 +82,23 @@ void dig() {  // all motor movements are blocking
 	// Tube 1
 	// auger.setSpeed(AUGER_DROP_SPEED);
 	// delay(AUGER_DROP_DELAY);
-	// // auger.softBreak();
+	// auger.softBrake();
 	Serial.println("Press Enter when the drop is finished.");
 	block();
 
 	// Tube 2
 	dirtCarousel.nextTube();
-	// // auger.setSpeed(AUGER_DROP_SPEED);
+	// auger.setSpeed(AUGER_DROP_SPEED);
 	// delay(AUGER_DROP_DELAY);
-	// // auger.softBreak();
+	// auger.softBrake();
 	Serial.println("Press Enter when the drop is finished.");
 	block();
 
 	// Inner tube
 	dirtLinear.setPosition(DIRT_LINEAR_INNER_RADIUS);
-	// // auger.setSpeed(AUGER_DROP_SPEED);
+	// auger.setSpeed(AUGER_DROP_SPEED);
 	// delay(AUGER_DROP_DELAY);
-	// // auger.softBreak();
+	// auger.softBrake();
 	Serial.println("Press Enter when the drop is finished.");
 	block();
 
@@ -104,27 +106,26 @@ void dig() {  // all motor movements are blocking
 	// Tube 3
 	dirtLinear.setPosition(DIRT_LINEAR_OUTER_RADIUS);
 	dirtCarousel.nextTube();
-	// // auger.setSpeed(AUGER_DROP_SPEED);
+	// auger.setSpeed(AUGER_DROP_SPEED);
 	// delay(AUGER_DROP_DELAY);
-	// // auger.softBreak();
+	// auger.softBrake();
 	Serial.println("Press Enter when the drop is finished.");
 	block();
 
 	// // Tube 4
 	dirtCarousel.nextTube();
-	// // auger.setSpeed(AUGER_DROP_SPEED);
+	// auger.setSpeed(AUGER_DROP_SPEED);
 	// delay(AUGER_DROP_DELAY);
-	// // auger.softBreak();
+	// auger.softBrake();
 	Serial.println("Press Enter when the drop is finished.");
 	block();
 	dirtCarousel.nextSection();
-	// dirtCarousel.nextSection();
 
 	// // Reset
 	dirtLinear.setPosition(0);	
-	// // auger.setSpeed(AUGER_DROP_SPEED);
+	// auger.setSpeed(AUGER_DROP_SPEED);
 	// delay(AUGER_DROP_DELAY);
-	// auger.softBreak();
+	// auger.softBrake();
 	Serial.println("Press Enter when the excess is dropped.");
 	block();
 
@@ -132,20 +133,37 @@ void dig() {  // all motor movements are blocking
 }
 
 void testSamples() {
+	Serial.println("Moving dirt linear");
 	dirtLinear.setPosition(DIRT_LINEAR_TEST_OFFSET);
+	Serial.println("Moving science linear");
 	scienceLinear.setPosition(SCIENCE_LINEAR_MAX_HEIGHT);
-	// pump1.setSpeed(PUMP_SPEED);
-	// pump2.setSpeed(PUMP_SPEED);
-	// pump3.setSpeed(PUMP_SPEED);
-	// pump4.setSpeed(PUMP_SPEED);
-	// delay(PUMP_DELAY);
-	// pump1.hardBrake();
-	// pump2.hardBrake();
-	// pump3.hardBrake();
-	// pump4.hardBrake();
+
+	Serial.println("Pumping...");
+	pump1.setSpeed(PUMP_SPEED);
+	pump2.setSpeed(PUMP_SPEED);
+	pump3.setSpeed(PUMP_SPEED);
+	pump4.setSpeed(PUMP_SPEED);
+	delay(PUMP_DELAY);
+	pump1.hardBrake();
+	pump2.hardBrake();
+	pump3.hardBrake();
+	pump4.hardBrake();
+
 	Serial.println("Transmitting data. (not really)..");
 	delay(SENSORS_READ_DELAY);
 	scienceLinear.setPosition(0);
+}
+
+void bubbles() {
+	pump1.setSpeed(PUMP_SPEED);
+	pump2.setSpeed(PUMP_SPEED);
+	pump3.setSpeed(PUMP_SPEED);
+	pump4.setSpeed(PUMP_SPEED);
+	delay(PUMP_DELAY);
+	pump1.hardBrake();
+	pump2.hardBrake();
+	pump3.hardBrake();
+	pump4.hardBrake();
 }
 
 void parseSerialCommand(String input) {
@@ -155,6 +173,8 @@ void parseSerialCommand(String input) {
 	if (input == "calibrate") return calibrate();
 	else if (input  == "dig") return dig();
 	else if (input == "temp") return dirtCarousel.nextSection();
+	else if (input == "test") return testSamples();
+	else if (input == "bubbles") return bubbles();
 
 	// Parse the command
 	int delimiter = input.indexOf(" ");
@@ -171,10 +191,24 @@ void parseSerialCommand(String input) {
 	else if (motor == "science-linear") scienceLinear.moveDistance(distance);
 	else if (motor == "dirt-carousel") dirtCarousel.rotate(distance);
 	// else if (motor == "auger") auger.setSpeed(speed);
-	else if (motor == "pump1") pump1.setSpeed(speed);
-	else if (motor == "pump2") pump2.setSpeed(speed);
-	else if (motor == "pump3") pump3.setSpeed(speed);
-	else if (motor == "pump4") pump4.setSpeed(speed);
+	else if (motor == "pump1") {
+		pump1.setSpeed(speed);
+		delay(PUMP_DELAY);
+		pump1.hardBrake();
+	} else if (motor == "pump2") {
+		pump2.setSpeed(speed);
+		delay(PUMP_DELAY);
+		pump2.hardBrake();
+	} else if (motor == "pump3") {
+		pump3.setSpeed(speed);
+		delay(PUMP_DELAY);
+		pump3.hardBrake();
+	} else if (motor == "pump4") {
+		Serial.println(speed);
+		pump4.setSpeed(speed);
+		delay(PUMP_DELAY);
+		pump4.hardBrake();
+	}
 	else {
 		Serial.println("Command not recognized: " + input);
 		Serial.println("  Commands are of the form: motor-name distance/speed.");
@@ -185,5 +219,3 @@ void parseSerialCommand(String input) {
 		Serial.println("");
 	}
 }
-
-// 2550 steps / rotation
