9ce543d98f899e7b24ec9b0d9ff60cea01bbc38e
Levi Lesches <levilesches@gmail.com>
2023-05-05 00:59:04 -0400

    Simplify pH and don't calibrate on loop

--- Science/src/pH/src/pH.cpp
+++ Science/src/pH/src/pH.cpp
@@ -8,25 +8,17 @@ void pHSensor::setup(){
   pH.begin();
 }
 
-void pHSensor::sample_pH(){
-  static unsigned long timepoint = millis();
+float pHSensor::sample_pH(){
   float voltage = 0;
   float pHvalue = 0;
   float temperature = 25;
-    if(millis()-timepoint>1000U){                  //time interval: 1s
-        timepoint = millis();
-        temperature = humsensor.readTemperature();           // read your temperature sensor to execute temperature compensation
-        voltage = analogRead(pHPIN)/1024.0*5000;   // read the voltage
-        pHvalue = pH.readPH(voltage,temperature);  // convert voltage to pH with temperature compensation
-        Serial.print("temperature:");
-        Serial.print(temperature,1);
-        Serial.print("^C  pH:");
-        Serial.println(pHvalue,2);
-    }
-    pH.calibration(voltage,temperature);
-}
-
-float pHSensor::returnpH()
-{
+  temperature = humsensor.readTemperature();           // read your temperature sensor to execute temperature compensation
+  voltage = analogRead(pHPIN)/1024.0*5000;   // read the voltage
+  pHvalue = pH.readPH(voltage,temperature);  // convert voltage to pH with temperature compensation
+  Serial.print("temperature:");
+  Serial.print(temperature,1);
+  Serial.print("^C  pH:");
+  Serial.println(pHvalue,2);
   return pHvalue;
 }
+
