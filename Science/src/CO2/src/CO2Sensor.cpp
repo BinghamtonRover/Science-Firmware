01687b1f2448c9d52d107320f784a36efb42db40
Levi Lesches <levilesches@gmail.com>
2023-05-05 00:58:43 -0400

    Updated CO2 to not block the main loop

--- Science/src/CO2/src/CO2Sensor.cpp
+++ Science/src/CO2/src/CO2Sensor.cpp
@@ -1,23 +1,11 @@
 #include "CO2Sensor.h"
 
 void CO2Sensor::setup() {
-  pinMode(BOOL_PIN, INPUT);                        //set pin to input
-  digitalWrite(BOOL_PIN, HIGH);
-}
-
-float CO2Sensor::MGRead() {
-  for (int i=0; i<READ_SAMPLE_TIMES; i++) {
-    volts += analogRead(CO2_PIN);
-    delay(READ_SAMPLE_INTERVAL);
-  }
-  volts = (volts / READ_SAMPLE_TIMES) * 5 / 1024;
-  return volts;
+  pinMode(CO2_PIN, INPUT);
 }
 
 float CO2Sensor::getPercentage() {
-  if ((volts/DC_GAIN )>=ZERO_POINT_VOLTAGE) {
-    return -1;
-  } else {
-    return pow(10, ((volts / DC_GAIN) - CO2Curve[1]) / CO2Curve[2] + CO2Curve[0]);
-  }
+  float volts = (float) analogRead(CO2_PIN) * 5 / 1024;
+  if ((volts / DC_GAIN) >= ZERO_POINT_VOLTAGE) return -1;
+  return pow(10, (((volts / DC_GAIN) - CO2Curve[1]) / CO2Curve[2] + CO2Curve[0]));
 }
--- Science/src/CO2/src/CO2Sensor.h
+++ Science/src/CO2/src/CO2Sensor.h
@@ -4,31 +4,21 @@
 #include <Arduino.h>
 
 #define         CO2_PIN                       17     //define which analog input channel you are going to use
-#define         BOOL_PIN                     (2)
 #define         DC_GAIN                      (8.5)   //define the DC gain of amplifier
-
-/***********************Software Related Macros************************************/
-#define         READ_SAMPLE_INTERVAL         (100)    //define how many samples you are going to take in normal operation
-#define         READ_SAMPLE_TIMES            (1)     //define the time interval(in milisecond) between each samples in
-                                                     //normal operation
 
 /**********************Application Related Macros**********************************/
 //These two values differ from sensor to sensor. user should derermine this value.
-#define         ZERO_POINT_VOLTAGE           (0.406) //define the output of the sensor in volts when the concentration of CO2 is 400PPM
+#define         ZERO_POINT_VOLTAGE           (0.435) //define the output of the sensor in volts when the concentration of CO2 is 400PPM
 #define         REACTION_VOLTAGE             (0.05) //define the voltage drop of the sensor when move the sensor from air into 1000ppm CO2
 
 /*****************************Globals***********************************************/
 
 class CO2Sensor{
   public:
-    float MGRead();
     float getPercentage();
     void setup();
 
   private:
-    int percentage;
-    float volts;
-
     /// Two points are taken from the curve to form a line that is approximately equivalent to the 
     /// original curve.
     /// 
