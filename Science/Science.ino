2cf00e260d36e66d9ee25786259545612bf79bb9
Levi Lesches <levilesches@gmail.com>
2023-05-22 22:06:00 -0400

    Updated TMC calibration logic

--- Science/Science.ino
+++ Science/Science.ino
@@ -31,6 +31,8 @@
 #define SCIENCE_COMMAND_ID 0x43
 #define SCIENCE_DATA_ID 0x17
 
+#define USE_SERIAL_MONITOR false
+
 void scienceHandler(const uint8_t* data, int length);
 BurtSerial serial(scienceHandler, Device::Device_SCIENCE);
 BurtCan can(SCIENCE_COMMAND_ID, scienceHandler);
@@ -47,6 +49,8 @@ HumiditySensor humsensor = HumiditySensor(HUM_PIN);
 CO2Sensor co2 = CO2Sensor();
 pHSensor pH = pHSensor(PH_PIN, humsensor);
 
+ScienceState state = ScienceState_STOP_COLLECTING;
+
 int canSendInterval = 200;
 unsigned long nextSendTime;
 
@@ -54,13 +58,15 @@ void setup() {
 	Serial.begin(9600);
   Serial.println("Initializing...");
 
+  Serial.println("Initializing vacuum...");
   vacuum.setup();
   dirtRelease.setup();
 
+  Serial.println("Initializing communications...");
   can.setup();
   nextSendTime = millis() + canSendInterval;
 
-  //setting up stepper motors
+  Serial.println("Initializing stepper motors...");
   vacuumLinear.presetup();
   dirtLinear.presetup();
   scienceLinear.presetup();
@@ -71,26 +77,22 @@ void setup() {
   scienceLinear.setup();
   dirtCarousel.setup();
 
-  //vacuumLinear.calibrate();
-  //dirtLinear.calibrate();
-  //scienceLinear.calibrate();
-  //dirtCarousel.calibrate();
+  Serial.println("Calibrating motors...");
+  vacuumLinear.calibrate();
+  scienceLinear.calibrate();
+  dirtLinear.calibrate();
+  dirtCarousel.calibrate();
 
-	Serial.println("Stepper motors initialized.");
+  Serial.println("Initializing pumps...");
+  pump1.setup();
+  pump2.setup();
+  pump3.setup();
+  pump4.setup();
 
-  // //TODO: Determine which pump is which (labelled D-G on hardware)
-	 pump1.setup();
-	 pump2.setup();
-	 pump3.setup();
-	 pump4.setup();
-	 Serial.println("DC motors initialized.");
-
-	// Serial.println("Vacuum initialized.");
-
+  Serial.println("Initializing sensors...");
   humsensor.setup();
   co2.setup();
   pH.setup();
-  Serial.println("Sensors initialized.");
 
 	Serial.println("Science Subsystem ready.");
 }
@@ -103,22 +105,15 @@ void loop() {
   dirtCarousel.update();
 
   can.update();
-  serial.update();
   sendData();
 
-  /* Temporary Serial Monitor interface */
-  // String input = Serial.readString();
-  // parseSerialCommand(input);
-  // delay(10);
+  if (USE_SERIAL_MONITOR) parseSerialCommand();
+  else serial.update();
 }
 
 /* Temporary Serial Monitor interface for testing. */
-void parseSerialCommand(String input) {
-  // Accept the command as-is
-  // if (input == "calibrate") return calibrate();
-  // else if (input  == "dig") return dig();
-  // else if (input == "test") return testSamples();
-  // Parse the command
+void parseSerialCommand() {
+  String input = Serial.readString();
   int delimiter = input.indexOf(" ");
   if (delimiter == -1) return;
   String part1 = input.substring(0, delimiter);
@@ -128,14 +123,10 @@ void parseSerialCommand(String input) {
   int speed = part2.toInt();
 
   // Execute the command
-  if (motor == "vacuum-linear") {
-    Serial.println("Moving vacuum linear");
-    vacuumLinear.debugMoveBySteps(distance);
-  }
-  
-  else if (motor == "dirt-linear") dirtLinear.debugMoveBySteps(distance); //dirtLinear.moveBy(distance);  
-  else if (motor == "science-linear") scienceLinear.debugMoveBySteps(distance); //scienceLinear.moveBy(distance);  
-  else if (motor == "dirt-carousel") dirtCarousel.debugMoveBySteps(distance); //dirtCarousel.moveBy(distance);  
+  if (motor == "vacuum-linear") vacuumLinear.moveTo(distance);  
+  else if (motor == "dirt-linear") dirtLinear.moveTo(distance); //dirtLinear.moveBy(distance);  
+  else if (motor == "science-linear") scienceLinear.moveTo(distance); //scienceLinear.moveBy(distance);  
+  else if (motor == "dirt-carousel") dirtCarousel.moveTo(distance); //dirtCarousel.moveBy(distance);  
   else if (motor == "vacuum_on") vacuum.enable();
   else if (motor == "vacuum_off") vacuum.disable();
   else if (motor == "dirt-release") dirtRelease.moveBy(distance); //go +49 to uncover hole, -49 to go back
@@ -156,8 +147,7 @@ void parseSerialCommand(String input) {
     pump4.setSpeed(speed);
     delay(PUMP_DELAY);
     pump4.hardBrake();
-  }
-  else {
+  } else {
     Serial.println("Command not recognized: " + input);
     Serial.println("  Commands are of the form: motor-name distance/speed.");
     Serial.println("  For stepper motors: positive distance is away from the limit switch.");
@@ -168,36 +158,22 @@ void parseSerialCommand(String input) {
   }
 }
 
-void updatePump(DCMotor pump, PumpState state) {
-  switch (state) {
-    case PumpState::PumpState_PUMP_STATE_UNDEFINED: return;
-    case PumpState::PumpState_PUMP_ON: pump.setSpeed(-100); break;
-    case PumpState::PumpState_PUMP_OFF: pump.setSpeed(0); break;
-  }
-}
-
 //Will need to switch to TMC commands once we have tested them
 void scienceHandler(const uint8_t* data, int length) {
   ScienceCommand command = BurtProto::decode<ScienceCommand>(data, length, ScienceCommand_fields);
   // Individual motor control
-  dirtCarousel.debugMoveBySteps(command.dirt_carousel);
-  dirtLinear.debugMoveBySteps(command.dirt_linear);
-  scienceLinear.debugMoveBySteps(command.science_linear);
-  vacuumLinear.debugMoveBySteps(command.vacuum_linear);
-
-  // We get multiple packets without vacuum data, so [vacuumPower] will be 0. This will happen even
-  // when the user is driving the vacuum, but we don't want to set the vacuum on and off all the 
-  // time. So we'll ignore [vacuumPower] unless [set_vacuum] is true.
-  if (command.vacuum == PumpState_PUMP_ON) vacuum.enable();
-  else if (command.vacuum == PumpState_PUMP_OFF) vacuum.disable();
-  if (command.dirtRelease == DirtReleaseState_OPEN_DIRT) dirtRelease.open();
-  else if(command.dirtRelease == DirtReleaseState_CLOSE_DIRT) dirtRelease.close();
+  dirtCarousel.moveBySteps(command.dirt_carousel);
+  dirtLinear.moveBySteps(command.dirt_linear);
+  scienceLinear.moveBySteps(command.science_linear);
+  vacuumLinear.moveBySteps(command.vacuum_linear);
 
   // Pumps
-  updatePump(pump1, command.pump1);
-  updatePump(pump2, command.pump2);
-  updatePump(pump3, command.pump3);
-  updatePump(pump4, command.pump4);
+  pump1.handleCommand(command.pump1);
+  pump2.handleCommand(command.pump2);
+  pump3.handleCommand(command.pump3);
+  pump4.handleCommand(command.pump4);
+  vacuum.handleCommand(command.vacuum);
+  dirtRelease.handleCommand(command.dirtRelease);
 
   // Commands
   if (command.stop) {
@@ -211,17 +187,17 @@ void scienceHandler(const uint8_t* data, int length) {
     pump3.setSpeed(0);
     pump4.setSpeed(0);
   } else if (command.calibrate) {
-    // dirtCarousel.calibrate();
-    // dirtLinear.calibrate();
-    // scienceLinear.calibrate();
-    // vacuumLinear.calibrate();
+    dirtCarousel.calibrate();
+    dirtLinear.calibrate();
+    scienceLinear.calibrate();
+    vacuumLinear.calibrate();
   }
   if (command.next_tube) dirtCarousel.moveBy(PI / 6); 
   if (command.next_section) dirtCarousel.moveBy(2 * PI / 3);
 }
 
 void sendData() {
-  if (millis() < nextSendTime) return;
+  if (state != ScienceState_COLLECT_DATA || millis() < nextSendTime) return;
   ScienceData data = ScienceData_init_zero;
   data.methane = methanesensor.getMethanePPM();
   can.send(SCIENCE_DATA_ID, &data, ScienceData_fields);
