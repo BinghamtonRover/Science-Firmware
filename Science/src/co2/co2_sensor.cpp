d7687cb5496f8e6c33be77abec72e53320f0679f
BURT Laptop <rover@binghamton.edu>
2025-02-04 20:02:49 -0500

    Added EZO I2C Final C02 Sensor working code

--- Science/src/co2/co2_sensor.cpp
+++ Science/src/co2/co2_sensor.cpp
@@ -1,21 +1,69 @@
 #include "co2_sensor.h"
+#define time_ 500
+#define WIRE Wire1
 
-Co2Sensor::Co2Sensor(int pin) : pin(pin) { }
+Co2Sensor::Co2Sensor(int address) : address(address) {}
 
-void Co2Sensor::setup() {
-  pinMode(pin, INPUT);
+void Co2Sensor::setup()
+{
+  WIRE.begin();
 }
 
-float Co2Sensor::read() { 
-  // Old logic:
-  float voltageOld = (float) analogRead(pin) * 5 / 1024 / CO2_GAIN;
-  if (voltageOld >= CO2_OLD_VOLTAGE_400) return -1;
-  float oldValue = pow(10, ((voltageOld - CO2_OLD_1) / CO2_OLD_2 + CO2_OLD_0));
+float Co2Sensor::read()
+{
+  byte code = 0;
+  byte in_char = 0;
+  int i = 0;
+  char Co2_data[20];
+  int Co2_int = 0;
 
-  // New logic:
-  // float voltage = (float) analogRead(pin) / 1024 * 5;
-  // float exponent = (voltage - CO2_LOWER_VOLTAGE) * log10(CO2_LOWER_PPM / CO2_UPPER_PPM) / CO2_VOLTAGE_DROP / CO2_GAIN;
-  // float newValue = CO2_LOWER_PPM * pow(10, exponent);
+  // i2c implementation
+  WIRE.beginTransmission(address);  //call the circuit by its ID number.
+  WIRE.write('r');
+  WIRE.endTransmission();
 
-  return oldValue / 10 * 3.14;
+  // if (strcmp(computerdata, "sleep") != 0) {  //if the command that has been sent is NOT the sleep command, wait the correct amount of time and request data.
+                                             //if it is the sleep command, we do nothing. Issuing a sleep command and then requesting data will wake the Co2 sensor.
+
+    delay(time_);  //wait the correct amount of time for the circuit to complete its instruction.
+
+    WIRE.requestFrom(address, 20, 1);  //call the circuit and request 20 bytes (this may be more than we need)
+    code = WIRE.read();                //the first byte is the response code, we read this separately.
+
+    switch (code) {                 //switch case based on what the response code is.
+      case 1:                       //decimal 1.
+        Serial.println("Success");  //means the command was successful.
+        break;                      //exits the switch case.
+
+      case 2:                      //decimal 2.
+        Serial.println("Failed");  //means the command has failed.
+        break;                     //exits the switch case.
+
+      case 254:                     //decimal 254.
+        Serial.println("Pending");  //means the command has not yet been finished calculating.
+        break;                      //exits the switch case.
+
+      case 255:                     //decimal 255.
+        Serial.println("No Data");  //means there is no further data to send.
+        break;                      //exits the switch case.
+    }
+
+
+    Serial.print("Available Bytes: ");
+    Serial.println(WIRE.available());
+    while (WIRE.available()) {  //are there bytes to receive.
+      in_char = WIRE.read();
+      Co2_data[i] = in_char;  //load this byte into our array.
+      i += 1;                 //incur the counter for the array element.
+      if (in_char == 0) {     //if we see that we have been sent a null command.
+        i = 0;                //reset the counter i to 0.
+        break;                //exit the while loop.
+      }
+    }
+  // }
+
+  //Uncomment this section if you want to take the Co2 value and convert it into integer number.
+  Co2_int = atoi(Co2_data);
+  return Co2_int;
+
 }
--- Science/src/co2/co2_sensor.h
+++ Science/src/co2/co2_sensor.h
@@ -2,6 +2,10 @@
 #define CO2Sensor_H
 
 #include <Arduino.h>
+#include <Wire.h>
+
+
+
 
 /* See the logic at https://www.digikey.com/htmldatasheets/production/2066424/0/0/1/sen0159.pdf 
 
@@ -10,26 +14,16 @@
   is good, but leads to an error of +/- 50ppm. See the link below for the sensor's sensitivity:
   https://image.dfrobot.com/image/data/SEN0159/CO2b%20MG811%20datasheet.pdf
 */
-#define CO2_GAIN          8.5     // The DC gain of the sensor
-#define CO2_LOWER_PPM     400     // The sensor has a lower bound of 400ppm
-#define CO2_UPPER_PPM     1000    // The sensor has an upper bound of 1,000 ppm
-#define CO2_LOWER_VOLTAGE 7  // voltage reading at STANDARD_PPM
-#define CO2_VOLTAGE_DROP  0.05    // The voltage drop from LOWER_PPM to UPPER_PPM
 
 class Co2Sensor {
   private: 
-    int pin;
+    int address;
 
   public:
-    Co2Sensor(int pin);
+    Co2Sensor(int addresss);
     void setup();
     float read();
 };
 
-// The old constants. TODO: Remove these in favor of above.
-#define CO2_OLD_0 2.602
-#define CO2_OLD_1 0.435
-#define CO2_OLD_2 -0.1256
-#define CO2_OLD_VOLTAGE_400 0.435
 
 #endif
