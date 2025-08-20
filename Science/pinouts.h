a96b349ae0869e0bdad60952bc266cce4070c119
nturner@binghamton.edu <njt112233@gmail.com>
2025-08-20 18:55:21 -0400

    Added carousel subsurface servo

--- Science/pinouts.h
+++ Science/pinouts.h
@@ -15,7 +15,8 @@ const int servo1 = 25;
 const int co2_address = 105;
 
 Motors motors(dirtCarouselMotor);
-SubSurfaceBox subSurface;
+SubSurfaceBox subSurfaceServo;
+SubSurfaceBox corouselServo;
 DirtCarousel carousel(dirtCarouselMotor);
 
 TempHumiditySensor tempHumidity;
