9635281421dade9d38c098e55f435db30da606f1
kenneth ou <kennethdacreator@gmail.com>
2026-02-18 18:36:56 -0500

    Move stepper pinouts from motor_pinouts to pinouts

--- Science-Aux/pinouts.h
+++ Science-Aux/pinouts.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "src/auger_servo/auger_servo.h"
 #include "src/temp_humidity/temp_humidity.h"
 #include "src/subsurface/subsurface.h"
 #include "src/TMC-Firmware/BURT_TMC.h"
@@ -6,50 +7,11 @@
 
 // -------------------- Pinouts --------------------
 
-const int servo1PIN = 10;
-const int servo2PIN = 3;
+const int upper_servo_pin = 10;
+const int lower_servo_pin = 3;
 
-SubSurfaceBox servo1(servo1PIN);
-SubSurfaceBox servo2(servo2PIN);
-
-/*struct StepperMotorPins {
-  int enable;
-  int chipSelect;
-};
-
-struct StepperMotorConfig {
-  String name;
-  int current;
-  int speed;
-  int acceleration;
-  double stepsPerUnit;
-};
-
-StepperMotorPins stepper1PINS = {0,15};  // 7, 5
-StepperMotorPins stepper2PINS = {0,14};  // 2, 6
-
-
-StepperMotorConfig stepper1CONFIG = { // arm swivel configs
-    "step1",
-    2000,
-    200'000,
-    200'000,
-    microstepsPerRadian * 47
-};
-
-StepperMotorConfig stepper2CONFIG = {
-    "step2",
-    2000,
-    200'000,
-    200'000,
-    microstepsPerRadian * 47
-};
-
-StepperMotor stepper1(stepper1PINS, stepper1CONFIG);
-StepperMotor stepper2(stepper2PINS, stepper2CONFIG);
-*/
-
-// TODO: Initialize refactored Steppers
+AugerServo upper_servo(upper_servo_pin);
+AugerServo lower_servo(lower_servo_pin);
 
 uint8_t reg = 0x00;
 uint8_t addr = 0x40;
@@ -57,3 +19,44 @@ uint8_t addr = 0x40;
 TempHumiditySensor tempHumidity(reg, addr);
 
 CurrentSensor currentSensor(23);
+
+// --------------- Dirt Linear  ---------------
+
+StepperGeneralConfig linearSliderGeneralConfig ={
+    name: "linearSlider",
+    steps_per_unit: microsteps_per_deg,
+};
+
+StepperMotorPins linearSliderPins = {
+	chip_select: 5, 
+};
+
+InternalRampConfig linearSliderConfig = {
+	current: 250, 
+	speed: 100'000,
+	acceleration: 200'000,
+};
+
+// LimitSwitch dirtLinearLimit = {
+// 	pin: 2, 
+// 	triggeredValue: HIGH,
+// 	direction: 1,
+// 	position: 0,
+// 	maxLimit: INFINITY,
+// };
+
+StepperMotor linearSlider(linearSliderGeneralConfig, linearSliderPins, linearSliderConfig);
+
+// // ---------------  Auger  ---------------
+
+StepperMotorPins augerMotorPins = {
+	enable: 8,
+};
+
+StepDirConfig augerMotorConfig = {
+	current: 250, 
+	speed: 20'000,
+	acceleration: 200'000,
+};
+
+StepperMotor augerMotor(augerMotorPins, augerMotorConfig);
